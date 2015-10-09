9da697cf023871e27ba6a5cd118b417600f01946
Kyle Garrett <kgarrett98@att.net>
2015-10-09 08:53:53 -0400

    Update

--- firstgitproject.py
+++ firstgitproject.py
@@ -2,25 +2,16 @@ __author__ = 'darrell.skogman'
 def addemup(a, b, c):
     return a+b+c
 
-def subtract(a, b, c):
-    return a-b-c
+def divideemup(a, b):
+    return a/b
     
-def multiply(a,b,c):
-    return a*b*c
-
-def average(a,b,c):
-    return a+b+c/3
-
 def main():
     a = int(input("first number "))
     b = int(input("second number "))
     c = int(input("third number "))
 
     print(addemup(a, b, c))
-    print(subtract(a, b, c))
-    print(multiply(a,b,c))
-    print(average(a,b,c))
-
+    print(divideemup(a, b))
 main()
 
 
