08967d600217e976f600620035767f83a6bf36c5
chsprogrammer <darrell.skogman@hallco.org>
2015-10-09 08:25:47 -0400

    A first attempt at posting commits

---
+++ firstgitproject.py
@@ -0,0 +1,16 @@
+__author__ = 'darrell.skogman'
+def addemup(a, b, c):
+    return a+b+c
+
+#PUT YOUR FUNCTION HERE
+
+def main():
+    a = int(input("first number "))
+    b = int(input("second number "))
+    c = int(input("third number "))
+
+    print(addemup(a, b, c))
+
+main()
+
+
